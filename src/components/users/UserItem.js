ad13960e82eb71a195ac1df4801423dcb69a0366
Cody Koscielski <codykoscielski@gmail.com>
2021-07-27 15:37:00 -0400

    Added a users and user item component to render data when fetched

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,16 +2,9 @@ import React, { Component } from 'react'
 
 class UserItem extends Component {
 
-    state = {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojobo'
-    };
-
     render() {
 
-        const { login, avatar_url, html_url } = this.state;
+        const { login, avatar_url, html_url } = this.props.user;
 
         return (
             <div className="card text-center">
@@ -22,7 +15,6 @@ class UserItem extends Component {
                     style={{ width: '60px'}}
                 />
                 <h3>{login}</h3>
-
                 <div>
                     <a href={html_url} className='btn btn-dark btn-sm my-1'>
                         more
