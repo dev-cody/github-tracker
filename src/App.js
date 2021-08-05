cb6a494b2319dedb0212ba7a294dea53fd73aa04
dev-cody <60411570+dev-cody@users.noreply.github.com>
2021-08-04 21:01:40 -0400

    Added comments, created a clear function to remove searched users

--- src/App.js
+++ src/App.js
@@ -7,40 +7,47 @@ import Users from './components/users/Users';
 import Search from './components/users/Search';
 
 class App extends Component {
-
+  //Setting the inital state
   state = {
     users: [],
     loading: false
   }
 
+  //Pull a random 30 users when component mounts
   async componentDidMount() {
-
+    //Setting the state to show the loading spinner
     this.setState({ loading: true });
-
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    
+    //Basic fetch to the first 30 random github users
+    const res = await axios.get(`https://api.github.com/users?
+    client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    //Setting the state of users to res.data and then removing the spiiner
     this.setState({ users: res.data, loading: false});
   }
 
   //search github users
   searchUsers = async (text) => {
-
+    //Set the spinner to be loading again
     this.setState({ loading: true });
-
+    //Fetch the data from github with a search query
     const res = await axios.get(`https://api.github.com/search/users?q=${text}
     &client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
     &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
+    //Setting the users to res.data.items and spiiner to false
     this.setState({ users: res.data.items, loading: false});
   }
-  
+
+  //Clear users from state 
+  clearUsers = () => this.setState({ users: [], loading: false});
+
+  //Render the components to the DOM
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          
-          <Search searchUsers={ this.searchUsers }/>
+          {/* Props being passed up from the Seach component */}
+          <Search searchUsers={ this.searchUsers } clearUsers={ this.clearUsers }/>
           <Users loading={ this.state.loading } users={ this.state.users }/>
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
+//Search component
 class Search extends Component {
-
+    //Setting the inital state
     state = {
         text: ''
     }
-
+    //Setting the prop types
     static propTypes = {
-        searchUsers: PropTypes.func.isRequired
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired
     }
 
     onChange = (e) => this.setState({ [e.target.name]: e.target.value })
     
     onSubmit = (e) => {
+        //Preventing the from from loading
         e.preventDefault();
         //Passing the text state to the main app component
         this.props.searchUsers(this.state.text);
@@ -28,6 +31,7 @@ class Search extends Component {
                     <input type="text" name="text" placeholder="Search Users" value={ this.state.text } onChange={ this.onChange }/>
                     <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
+                <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>
             </div>
         )
     }
