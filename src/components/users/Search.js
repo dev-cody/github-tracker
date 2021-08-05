955e5c4d9f7b86ec35f8337ed7d0646ff8ebc6c2
dev-cody <60411570+dev-cody@users.noreply.github.com>
2021-08-04 20:42:10 -0400

    Added the search functionality by passing up props

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 class Search extends Component {
+
+    state = {
+        text: ''
+    }
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired
+    }
+
+    onChange = (e) => this.setState({ [e.target.name]: e.target.value })
+    
+    onSubmit = (e) => {
+        e.preventDefault();
+        //Passing the text state to the main app component
+        this.props.searchUsers(this.state.text);
+        //Setting the text back to nothing
+        this.setState({ text: ''});
+    }
+
     render() {
         return (
             <div>
-                <form className="form">
-                    <input type="text" name="text" placeholder="Search Users"/>
-                    <input type="submit" value="search" className="btn btn-dark btn-block" />
+                <form className="form" onSubmit={ this.onSubmit }>
+                    <input type="text" name="text" placeholder="Search Users" value={ this.state.text } onChange={ this.onChange }/>
+                    <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
             </div>
         )
     }
 }
+
+
 
 export default Search
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -13,7 +13,7 @@ const UserItem = ({user: { login, avatar_url, html_url }}) => {
             />
             <h3>{login}</h3>
             <div>
-                <a href={html_url} className='btn btn-dark btn-sm my-1'>
+                <a href={html_url} target="_blank" className='btn btn-dark btn-sm my-1'>
                     more
                 </a>
             </div>
