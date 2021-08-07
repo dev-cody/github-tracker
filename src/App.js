44f26df747c8494d0a9d44c16d4baaf2dd40e848
dev-cody <60411570+dev-cody@users.noreply.github.com>
2021-08-06 20:47:09 -0400

    Started to set up react router dom, added routes for home and about

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
 
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 
 class App extends Component {
   //Setting the inital state
@@ -16,16 +18,16 @@ class App extends Component {
   }
 
   //Pull a random 30 users when component mounts
-  // async componentDidMount() {
-  //   //Setting the state to show the loading spinner
-  //   this.setState({ loading: true });
-  //   //Basic fetch to the first 30 random github users
-  //   const res = await axios.get(`https://api.github.com/users?
-  //   client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-  //   &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-  //   //Setting the state of users to res.data and then removing the spiiner
-  //   this.setState({ users: res.data, loading: false});
-  // }
+  async componentDidMount() {
+    //Setting the state to show the loading spinner
+    this.setState({ loading: true });
+    //Basic fetch to the first 30 random github users
+    const res = await axios.get(`https://api.github.com/users?
+    client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    //Setting the state of users to res.data and then removing the spiiner
+    this.setState({ users: res.data, loading: false});
+  }
 
   //search github users
   searchUsers = async (text) => {
@@ -58,20 +60,29 @@ class App extends Component {
     const {users, loading} = this.state;
 
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={ this.state.alert }/>
-          {/* Props being passed up from the Seach component */}
-          <Search 
-            searchUsers={ this.searchUsers } 
-            clearUsers={ this.clearUsers } 
-            showClear={ users.length > 0 ? true : false } 
-            setAlert={ this.setAlert }
-          />
-          <Users loading={ loading } users={ users }/>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={ this.state.alert }/>
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                  {/* Props being passed up from the Seach component */}
+                  <Search 
+                    searchUsers={ this.searchUsers } 
+                    clearUsers={ this.clearUsers } 
+                    showClear={ users.length > 0 ? true : false } 
+                    setAlert={ this.setAlert }
+                  />
+                  <Users loading={ loading } users={ users }/>
+                </Fragment>
+              )} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
